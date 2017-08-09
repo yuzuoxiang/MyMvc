102a6d9b1f28765d54eb29132a0ed04f348b1d4f
余作想 <963526232@qq.com>
2017-08-09 15:22:33 +0800

    123

--- MyMvc.测试/Properties/AssemblyInfo.cs
+++ MyMvc.测试/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MyMvc")]
+[assembly: AssemblyTitle("MyMvc.测试")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MyMvc")]
+[assembly: AssemblyProduct("MyMvc.测试")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("e17e1bb3-411f-40c0-8af1-b1bd070cf21e")]
+[assembly: Guid("d1f100e5-f67e-48bb-bb0c-6eac77e04b0c")]
 
 // 程序集的版本信息由下列四个值组成:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      内部版本号
 //      修订版本
 //
-// 可以指定所有值，也可以使用“修订号”和“内部版本号”的默认值，
+// 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用 "*":
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyMvc.测试/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyMvc.测试
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
